861c04c24debe185476c5108c6b802f45c78b6ae
Utkarsh Gupta <ut.gupta29@gmail.com>
2023-05-05 19:37:33 +0530

    ex 3

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-// // console.log("task to understand js ")
-// var userName=prompt("You can write anything");
-// var welcome="hey there i know your name is that you???"+userName;
-// alert(welcome);
+
+// var myBrowser=document.querySelector("#my-Browser");
+
+//way1 to add function
+// myBrowser.addEventListener("click", myFunction);
+// function myFunction(){
+
+//    console.log("clicked!")
+// }
+// way2 to add function
+// myBrowser.addEventListener("click",function clickEventListener(){
+//     console.log("clicked!!")
+// })
