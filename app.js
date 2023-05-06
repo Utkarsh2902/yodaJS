8c1ee072736e8811cb3136f7b935b50398e071d5
Utkarsh Gupta <ut.gupta29@gmail.com>
2023-05-07 00:44:27 +0530

    ex4

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-
 // var myBrowser=document.querySelector("#my-Browser");
-
-//way1 to add function
-// myBrowser.addEventListener("click", myFunction);
-// function myFunction(){
-
-//    console.log("clicked!")
-// }
-// way2 to add function
-// myBrowser.addEventListener("click",function clickEventListener(){
-//     console.log("clicked!!")
-// })
+// var mytxt=document.querySelector("#my-txt");
+// myBrowser.addEventListener("click",clickHandler)
+// function clickHandler(){
+//     console.log("clicked"+mytxt.value);
+//     }
